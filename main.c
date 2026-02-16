dc753df189bc535d2d7cc049590fe171bd6720d4
Okan Doğan <okandogan015@gmail.com>
2026-02-16 23:54:48 +0300

    Proje dosyalari eklendi

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    float batarya_sicakligi;
+    int sarj_yuzdesi;
+    char kapi_durumu;
+    int fren_pedali;
+
+    printf("--- ELEKTROMOBİL GÜVENLİK KONTROLÜ ---\n");
+
+    printf("Batarya Sıcaklığı (°C): ");
+    scanf("%f", &batarya_sicakligi);
+
+    if (batarya_sicakligi < -100 || batarya_sicakligi > 200) {
+        printf("HATA: Geçersiz sıcaklık değeri.\n");
+        return 0;
+    }
+
+    printf("Şarj Yüzdesi (%%): ");
+    scanf("%d", &sarj_yuzdesi);
+
+    if (sarj_yuzdesi < 0 || sarj_yuzdesi > 100) {
+        printf("HATA: Hatalı sensör verisi. İmkansız şarj seviyesi.\n");
+        return 0;
+    }
+
+    printf("Kapı Durumu (A/K): ");
+    scanf(" %c", &kapi_durumu);
+
+    if (kapi_durumu != 'A' && kapi_durumu != 'K') {
+        printf("HATA: Geçersiz kapı durumu.\n");
+        return 0;
+    }
+
+    printf("Fren Pedalı (1:Basılı, 0:Değil): ");
+    scanf("%d", &fren_pedali);
+
+    if (fren_pedali != 0 && fren_pedali != 1) {
+        printf("HATA: Geçersiz fren pedalı verisi.\n");
+        return 0;
+    }
+
+    if (batarya_sicakligi > 60) {
+        printf("KRİTİK HATA: Motor Aşırı Isındı! Sürüş Engellendi.\n");
+        return 0;
+    }
+
+    if (sarj_yuzdesi < 10) {
+        printf("UYARI: Batarya Kritik Seviyede! Sürüş Başlatılamaz.\n");
+        return 0;
+    }
+
+    if (kapi_durumu == 'A') {
+        printf("HATA: Kapılar Açık! Lütfen Kapatınız.\n");
+        return 0;
+    }
+
+    if (fren_pedali == 0) {
+        printf("BİLGİ: Güvenlik için frene basarak tekrar deneyin.\n");
+        return 0;
+    }
+
+    printf("BAŞARILI: SİSTEM HAZIR. MOTOR BAŞLATILIYOR...\n");
+
+    return 0;
+}
