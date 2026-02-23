ae1c4670975e5e2a516dbcd2ef2e3e7006749f89
Okan Doğan <okandogan015@gmail.com>
2026-02-23 18:46:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main() {
     float batarya_sicakligi;
@@ -14,7 +13,7 @@ int main() {
 
     if (batarya_sicakligi < -100 || batarya_sicakligi > 200) {
         printf("HATA: Geçersiz sıcaklık değeri.\n");
-        return 0;
+        return 1;
     }
 
     printf("Şarj Yüzdesi (%%): ");
@@ -22,7 +21,7 @@ int main() {
 
     if (sarj_yuzdesi < 0 || sarj_yuzdesi > 100) {
         printf("HATA: Hatalı sensör verisi. İmkansız şarj seviyesi.\n");
-        return 0;
+        return 1;
     }
 
     printf("Kapı Durumu (A/K): ");
@@ -30,7 +29,7 @@ int main() {
 
     if (kapi_durumu != 'A' && kapi_durumu != 'K') {
         printf("HATA: Geçersiz kapı durumu.\n");
-        return 0;
+        return 1;
     }
 
     printf("Fren Pedalı (1:Basılı, 0:Değil): ");
@@ -38,27 +37,27 @@ int main() {
 
     if (fren_pedali != 0 && fren_pedali != 1) {
         printf("HATA: Geçersiz fren pedalı verisi.\n");
-        return 0;
+        return 1;
     }
 
     if (batarya_sicakligi > 60) {
         printf("KRİTİK HATA: Motor Aşırı Isındı! Sürüş Engellendi.\n");
-        return 0;
+        return 1;
     }
 
     if (sarj_yuzdesi < 10) {
         printf("UYARI: Batarya Kritik Seviyede! Sürüş Başlatılamaz.\n");
-        return 0;
+        return 1;
     }
 
     if (kapi_durumu == 'A') {
         printf("HATA: Kapılar Açık! Lütfen Kapatınız.\n");
-        return 0;
+        return 1;
     }
 
     if (fren_pedali == 0) {
         printf("BİLGİ: Güvenlik için frene basarak tekrar deneyin.\n");
-        return 0;
+        return 1;
     }
 
     printf("BAŞARILI: SİSTEM HAZIR. MOTOR BAŞLATILIYOR...\n");
